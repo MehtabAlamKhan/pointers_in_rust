1209965d19b68c5da1dc63a5f75692955c6d0d63
MehtabAlamKhan <kmehtabalam19@gmail.com>
2024-11-13 21:23:51 +0530

    rc

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-// mod ll;
+mod ll;
 // mod ll1;
 mod BinaryTree;
 
+use ll::ListNode;
 use std::borrow::BorrowMut;
-// use ll::ListNode;
 // use ll1::LinkedList;
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -11,16 +11,21 @@ use BinaryTree::TreeNode;
 
 fn main() {
     let mut tree = TreeNode::new(4);
-    pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
-        if root == None {
-            return root;
-        }
 
-        std::mem::swap(
-            root.unwrap().borrow().left.borrow_mut(),
-            root.unwrap().borrow().right.borrow_mut(),
-        );
+    let mut l1 = &mut Some(Box::new(ListNode::new(1)));
+    l1.as_mut().unwrap().next = Some(Box::new(ListNode::new(2)));
+    println!("{:?}", l1);
 
-        return root;
-    }
+    // pub fn invert_tree(root: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+    //     if root == None {
+    //         return root;
+    //     }
+
+    //     // std::mem::swap(
+    //     //     root.unwrap().borrow().left.borrow_mut(),
+    //     //     root.unwrap().borrow().right.borrow_mut(),
+    //     // );
+
+    //     return root;
+    // }
 }
